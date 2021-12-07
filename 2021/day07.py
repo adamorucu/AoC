d65deec08b4fb2b2eaaccff80c7c44d44abe26b8
adamorucu <adamdorucu@gmail.com>
2021-12-07 10:42:08 +0100

    better 7

--- 2021/day07.py
+++ 2021/day07.py
@@ -11,27 +11,24 @@ def calc_cost(crab, pos):
 
 m = defaultdict(int)
 for i in range(max(crabs) - min(crabs) + 1):
-    m[i] = sum(range(0, i+1))
+    m[i] = i*(i+1)//2
 
-min_cost = 9e+10
-best_pos = 9e+10
+min_cost = 9e10
 for pos in range(min(crabs), max(crabs)):
     cost = sum([calc_cost(c, pos) for c in crabs])
     if cost < min_cost:
         min_cost = cost
-        best_pos = pos
 
 print(min_cost)
 
 def calc_cost2(crab, pos):
     return m[abs(crab - pos)]
 
-min_cost = 9e+10
-best_pos = 9e+10
+min_cost = 9e10
 for pos in range(min(crabs), max(crabs)):
     cost = sum([calc_cost2(c, pos) for c in crabs])
     if cost < min_cost:
         min_cost = cost
-        best_pos = pos
 
 print(min_cost)
+
